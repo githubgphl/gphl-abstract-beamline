e81d221270876ddca7b9059b718a41c2ad303aec
Peter Keller <pkeller@globalphasing.com>
2015-01-30 18:14:23 +0000

    Update CollectionProposal as per UML diagram
     - Should have been turned into a subtype of IdentifiedElement ages
    ago....

--- com.globalphasing.sdcp.abstract_beamline/src/co/gphl/beamline/v2_unstable/data_collection/CollectionProposal.java
+++ com.globalphasing.sdcp.abstract_beamline/src/co/gphl/beamline/v2_unstable/data_collection/CollectionProposal.java
@@ -17,7 +17,9 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
-public interface CollectionProposal extends Serializable {
+import co.gphl.beamline.v2_unstable.IdentifiedElement;
+
+public interface CollectionProposal extends Serializable, IdentifiedElement {
 
     UUID getCollectionId();
     UUID getStrategyId();
