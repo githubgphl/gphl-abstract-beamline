183659d184e156285eb017dab90093604c8847e8
Peter Keller <pkeller@globalphasing.com>
2016-07-05 15:24:07 +0100

    Add timestamped (v1) UUID to issues

--- com.globalphasing.sdcp.abstract_beamline/src/co/gphl/beamline/v2_unstable/information/Issue.java
+++ com.globalphasing.sdcp.abstract_beamline/src/co/gphl/beamline/v2_unstable/information/Issue.java
@@ -12,14 +12,25 @@
  */
 package co.gphl.beamline.v2_unstable.information;
 
+import co.gphl.beamline.v2_unstable.IdentifiedElement;
+
 /**
  * @author pkeller
  *
  */
-public interface Issue {
+public interface Issue extends IdentifiedElement {
 
     String getComponent();
     int getCode();
     String getMessage();
+
+    /**
+     * Extracts the timestamp from the {@link #getId() id} and returns it
+     * converted to milliseconds since 1970-01-01T00:00.000Z.
+     * 
+     * @return timestamp
+     * @see java.lang.System#currentTimeMillis()
+     */
+    long getMilliTimestamp();
     
 }
