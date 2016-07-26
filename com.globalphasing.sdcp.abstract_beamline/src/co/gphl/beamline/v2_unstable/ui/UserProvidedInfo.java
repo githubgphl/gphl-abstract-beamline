79852b60f2b2fe05b7804362a45448339dc085f1
pkeller <pkeller@globalphasing.com>
2016-07-26 21:09:46 +0100

    Cater for user-specified crystal system

--- com.globalphasing.sdcp.abstract_beamline/src/co/gphl/beamline/v2_unstable/ui/UserProvidedInfo.java
+++ com.globalphasing.sdcp.abstract_beamline/src/co/gphl/beamline/v2_unstable/ui/UserProvidedInfo.java
@@ -2,9 +2,13 @@ package co.gphl.beamline.v2_unstable.ui;
 
 import java.util.List;
 
+import co.gphl.beamline.v2_unstable.domain_types.CrystalSystem;
+
 public interface UserProvidedInfo {
 
     short getSpaceGroup();
     float getExpectedResolution();
     List<PhasingWavelength> getPhasingWavelength();
+    boolean isSetLattice();
+	CrystalSystem getLattice();
 }
