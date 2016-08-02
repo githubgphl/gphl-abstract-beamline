0c346d5fce16579052676d3164638fdfa301ca86
Peter Keller <pkeller@globalphasing.com>
2016-08-02 14:10:30 +0100

    Add messages for starting and ending of workflow-side subprocesses
     - These are used to enable the beamline UI to handle standard output
    from these subprocesses better.

---
+++ com.globalphasing.sdcp.abstract_beamline/src/co/gphl/beamline/v2_unstable/information/SubprocessStarted.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright © 2016 by Global Phasing Ltd. All rights reserved
+ *
+ * This software is proprietary to and embodies the confidential
+ * technology of Global Phasing Limited (GPhL).
+ *
+ * Any possession or use (including but not limited to duplication, reproduction
+ * and dissemination) of this software (in either source or compiled form) is
+ * forbidden except where an agreement with GPhL that permits such possession or
+ * use is in force.
+ *
+ */
+package co.gphl.beamline.v2_unstable.information;
+
+/**
+ * @author pkeller
+ *
+ */
+public interface SubprocessStarted {
+
+    String getName();
+    
+}
