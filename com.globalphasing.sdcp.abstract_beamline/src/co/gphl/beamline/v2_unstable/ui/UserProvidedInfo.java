1752d29b896850544bb85678379cfffacb40f64c
Peter Keller <pkeller@globalphasing.com>
2017-03-22 17:37:27 +0000

    Add missing methods for UserProvidedInfo

--- com.globalphasing.sdcp.abstract_beamline/src/co/gphl/beamline/v2_unstable/ui/UserProvidedInfo.java
+++ com.globalphasing.sdcp.abstract_beamline/src/co/gphl/beamline/v2_unstable/ui/UserProvidedInfo.java
@@ -5,10 +5,13 @@ import java.util.Set;
 
 import co.gphl.beamline.v2_unstable.domain_types.AnomalousScatterer;
 import co.gphl.beamline.v2_unstable.domain_types.CrystalSystem;
+import co.gphl.beamline.v2_unstable.domain_types.UnitCell;
 
 public interface UserProvidedInfo {
 
     short getSpaceGroup();
+    UnitCell getCell();
+    boolean isSetCell();
     float getExpectedResolution();
     Set<PhasingWavelength> getPhasingWavelengths();
     boolean isSetLattice();
