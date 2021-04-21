1e0ffa2c1307fe029be11d6247c1959cfbd091b3
Peter Keller <pkeller@globalphasing.com>
2021-04-21 15:06:15 +0100

    SDCP-291 Allow BCS to convey more info to control strategy calculation
     - Cater for passing JSON string for interpretation by the stratcal
    wrapper
     - Wavelength and detector position for main data collection can now be
    updated before strategy calculation; no longer allowed after strategy
    calculation

--- com.globalphasing.sdcp.abstract_beamline/src/co/gphl/beamline/v2_unstable/information/SelectedLattice.java
+++ com.globalphasing.sdcp.abstract_beamline/src/co/gphl/beamline/v2_unstable/information/SelectedLattice.java
@@ -4,10 +4,21 @@ import java.util.List;
 
 import co.gphl.beamline.v2_unstable.Payload;
 import co.gphl.beamline.v2_unstable.domain_types.IndexingFormat;
+import co.gphl.beamline.v2_unstable.instrumentation.BcsDetectorSetting;
+import co.gphl.beamline.v2_unstable.ui.PhasingWavelength;
 
 public interface SelectedLattice extends Payload {
 
     IndexingFormat getFormat();
     List<String> getIndexingSolution();
+    
+    boolean isSetStrategyDetectorSetting();
+    BcsDetectorSetting getStrategyDetectorSetting();
+    
+    boolean isSetStrategyWavelength();
+    PhasingWavelength getStrategyWavelength();
+    
+    boolean isSetStrategyControl();
+    String getStrategyControl();
 
 }
